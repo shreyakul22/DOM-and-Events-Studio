5d92ac6fd77ac0fc87bec6e0c95e72f016397b62
Shreya <contact.shreya22@gmail.com>
2020-10-16 15:21:33 -0500

    first and second part

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+  });
+
+  function init () {
+      let takeOff = document.getElementById("takeoff");
+      console.log(typeof takeOff);
+      takeOff.addEventListener("click", function(event){
+          
+          if(window.confirm("ready for takeoff.")) {
+            document.getElementById("flightStatus").innerHTML="Shuttle in flight.";
+            document.getElementById("shuttleBackground").style.backgroundColor="blue";
+            document.getElementById("spaceShuttleHeight").innerHTML=10000;
+        };
+      }) 
+  }
+
+
+        
+
+
